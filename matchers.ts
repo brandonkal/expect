1f9cd2841f8158454eda6ad242eebdb33d24bcf0
Theo Sun <theo.sun@outlook.com>
2020-10-10 12:09:08 +0800

    chore: updates

--- matchers.ts
+++ matchers.ts
@@ -1,7 +1,7 @@
 import {
   AssertionError,
   equal,
-} from "https://deno.land/std@v0.50.0/testing/asserts.ts";
+} from "https://deno.land/std@0.69.0/testing/asserts.ts";
 
 import diff, {
   DiffResult,
