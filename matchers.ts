b622c2b22860b7e8fd73ea63de827cc73b0e6337
Theo Sun <theo.sun@outlook.com>
2020-10-10 13:07:54 +0800

    chore(deps): update

--- matchers.ts
+++ matchers.ts
@@ -6,14 +6,14 @@ import {
 import diff, {
   DiffResult,
   DiffType,
-} from "https://deno.land/std@v0.50.0/testing/diff.ts";
+} from "https://deno.land/std@0.69.0/testing/diff.ts";
 import {
   bold,
   gray,
   green,
   red,
   white,
-} from "https://deno.land/std@v0.50.0/fmt/colors.ts";
+} from "https://deno.land/std@0.69.0/fmt/colors.ts";
 
 import * as mock from "./mock.ts";
 
