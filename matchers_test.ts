51a0415d27ee5d8492b0ba2198d5fef70c689f0c
Theo Sun <theo.sun@outlook.com>
2020-11-28 10:33:53 +0800

    chore(deps): update

--- matchers_test.ts
+++ matchers_test.ts
@@ -1,7 +1,7 @@
 import {
   assert,
   assertEquals,
-} from "https://deno.land/std@0.69.0/testing/asserts.ts";
+} from "https://deno.land/std@0.79.0/testing/asserts.ts";
 import * as mock from "./mock.ts";
 
 import {
